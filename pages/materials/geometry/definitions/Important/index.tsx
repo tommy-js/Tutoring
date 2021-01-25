b9a7916cb00b04209fcfc79f14fcd7214bbb5920
Tyler Blovat <tyler.blovat@uconn.edu>
2021-01-25 10:27:43 -0500

    Add page routes to geometry

---
+++ pages/materials/geometry/definitions/Important/index.tsx
@@ -0,0 +1,11 @@
+import { Navbar } from "../../../../../components/navigation/Navbar/Navbar";
+import { Important } from "../../../../../components/materials/documents/geometry/definitions/Important/Important";
+
+export default function ImportantPage() {
+  return (
+    <div>
+      <Navbar />
+      <Important />
+    </div>
+  );
+}
---
+++ pages/materials/geometry/worksheets/Similar/index.tsx
@@ -0,0 +1,11 @@
+import { Navbar } from "../../../../../components/navigation/Navbar/Navbar";
+import { Similar } from "../../../../../components/materials/documents/geometry/worksheets/Similar/Similar";
+
+export default function SimilarPage() {
+  return (
+    <div>
+      <Navbar />
+      <Similar />
+    </div>
+  );
+}
