e4abc97eb055e37fdf664c2cffefc37f5a06e369
Tyler Blovat <tyler.blovat@uconn.edu>
2021-01-25 10:30:31 -0500

    Add precalc page routes

---
+++ pages/materials/precalc/worksheets/Complex/index.tsx
@@ -0,0 +1,11 @@
+import { Navbar } from "../../../../../components/navigation/Navbar/Navbar";
+import { Complex } from "../../../../../components/materials/documents/precalc/worksheets/Complex/Complex";
+
+export default function ComplexPage() {
+  return (
+    <div>
+      <Navbar />
+      <Complex />
+    </div>
+  );
+}
---
+++ pages/materials/precalc/worksheets/FunctionComposition/index.tsx
@@ -0,0 +1,11 @@
+import { Navbar } from "../../../../../components/navigation/Navbar/Navbar";
+import { FunctionComposition } from "../../../../../components/materials/documents/precalc/worksheets/FunctionComposition/FunctionComposition";
+
+export default function FunctionCompositionPage() {
+  return (
+    <div>
+      <Navbar />
+      <FunctionComposition />
+    </div>
+  );
+}
