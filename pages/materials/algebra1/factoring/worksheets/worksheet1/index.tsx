a5a785c884776cbec6525566601f70ea48118ff0
Tyler Blovat <tyler.blovat@uconn.edu>
2021-01-29 19:15:42 -0500

    Fix document loading issue within algebra1 page

--- pages/materials/algebra1/factoring/worksheets/worksheet1/index.tsx
+++ pages/materials/algebra1/factoring/worksheets/worksheet1/index.tsx
@@ -1,11 +1,11 @@
 import { Navbar } from "../../../../../../components/navigation/Navbar/Navbar";
-import { Worksheet1 } from "../../../../../../components/materials/documents/algebra1/factoring/Worksheet1/Worksheet1";
+import { Factoring } from "../../../../../../components/materials/documents/algebra1/worksheets/Factoring/Factoring";
 
 export default function FactoringWorksheet1Page() {
   return (
     <div>
       <Navbar />
-      <Worksheet1 />
+      <Factoring />
     </div>
   );
 }
