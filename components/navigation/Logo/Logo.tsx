d3e5e2dee5e4a39a8b04bb0c88cf7d31b4906ea1
Tyler Blovat <tyler.blovat@uconn.edu>
2021-01-29 19:12:19 -0500

    Fix typescript errors within navbar logo

--- components/navigation/Logo/Logo.tsx
+++ components/navigation/Logo/Logo.tsx
@@ -4,37 +4,27 @@ import styles from "./styles.module.scss";
 const logo = require("../../../public/logo.png");
 
 export const Logo: React.FC = () => {
+  const [width, setWidth] = useState("0");
   return (
     <div className={styles.logo}>
       <Link href="/" passHref>
-        <HomeLink />
+        <a
+          className={styles.link}
+          onMouseOver={() => setWidth("175px")}
+          onMouseOut={() => setWidth("0")}
+        >
+          <div className={styles.home_link}>
+            <div className={styles.image_container}>
+              <img src={logo} className={styles.image} />
+            </div>
+            <div className={styles.header_container}>
+              <p style={{ width }} className={styles.header}>
+                Functional Tutoring
+              </p>
+            </div>
+          </div>
+        </a>
       </Link>
     </div>
   );
 };
-
-const HomeLink = React.forwardRef(({ onClick, href }, ref) => {
-  const [width, setWidth] = useState("0");
-
-  return (
-    <a
-      className={styles.link}
-      href={href}
-      onClick={onClick}
-      ref={ref}
-      onMouseOver={() => setWidth("175px")}
-      onMouseOut={() => setWidth("0")}
-    >
-      <div className={styles.home_link}>
-        <div className={styles.image_container}>
-          <img src={logo} className={styles.image} />
-        </div>
-        <div className={styles.header_container}>
-          <p style={{ width }} className={styles.header}>
-            Functional Tutoring
-          </p>
-        </div>
-      </div>
-    </a>
-  );
-});
