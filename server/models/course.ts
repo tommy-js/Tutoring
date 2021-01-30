9cdaff2153b0332005089c8419299a6a9d3731ef
Tyler Blovat <tyler.blovat@uconn.edu>
2021-01-29 19:18:53 -0500

    Zero out all lines within the server

--- server/models/course.ts
+++ server/models/course.ts
@@ -1,9 +1,9 @@
-const mongoose = require("mongoose");
-const Schema = mongoose.Schema;
-
-const courseSchema = new Schema({
-  courseId: String,
-  title: String,
-});
-
-module.exports = mongoose.model("User", courseSchema);
+// const mongoose = require("mongoose");
+// const Schema = mongoose.Schema;
+//
+// const courseSchema = new Schema({
+//   courseId: String,
+//   title: String,
+// });
+//
+// module.exports = mongoose.model("User", courseSchema);
