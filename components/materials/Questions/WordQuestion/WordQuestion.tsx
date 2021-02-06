c0c9262ffe5362dc5d2dfe29870719632351e175
Tyler Blovat <tyler.blovat@uconn.edu>
2021-02-06 17:34:11 -0500

    Add standard math equation question

---
+++ components/materials/Questions/WordQuestion/WordQuestion.tsx
@@ -0,0 +1,46 @@
+import React, { useState } from "react";
+import ReactMarkdown from "react-markdown";
+import RemarkMathPlugin from "remark-math";
+import { BlockMath, InlineMath } from "react-katex";
+import "katex/dist/katex.min.css";
+import styles from "./styles.module.scss";
+
+const katex = require("katex");
+
+interface Props {
+  text: string;
+  function: string;
+  index: number;
+}
+
+export const WordQuestion = (props) => {
+  const [answer, setAnswer] = useState("");
+
+  function testKatex(equa: string) {
+    try {
+      var html = katex.renderToString(equa);
+      if (html) setAnswer(equa);
+    } catch (e) {
+      setAnswer(answer);
+    }
+  }
+  return (
+    <div className={styles.word_question}>
+      <p className={styles.question}>
+        {props.index}. {props.text}
+      </p>
+      <div className={styles.function}>
+        <InlineMath>{props.function}</InlineMath>
+      </div>
+      <div className={styles.answer}>
+        <input
+          onChange={(e) => testKatex(e.target.value)}
+          className={styles.answer_input}
+        />
+      </div>
+      <div className={styles.answer_block}>
+        <BlockMath>{answer}</BlockMath>
+      </div>
+    </div>
+  );
+};
--- components/materials/documents/algebra1/worksheets/Factoring/Factoring.tsx
+++ components/materials/documents/algebra1/worksheets/Factoring/Factoring.tsx
@@ -1,10 +1,17 @@
 import React from "react";
+import Link from "next/link";
+import { WordQuestion } from "../../../../Questions/WordQuestion/WordQuestion";
 import styles from "./styles.module.scss";
 
 export const Factoring: React.FC = () => {
   return (
     <div className={styles.factoring}>
-      <h1 className={styles.header}>Fractoring Practice</h1>
+      <h1 className={styles.header}>Factoring Practice</h1>
+      <Link href="/formatting">
+        <a>Need help formatting?</a>
+      </Link>
+      <WordQuestion index={1} text="Factor" function="(ax+by)(cd+ef)" />
+      <WordQuestion index={2} text="Factor" function="(x^2+y^3)(x^3+y)" />
     </div>
   );
 };
