9e9ac96a8b3fa488168f09434fc7a76accee6799
Tyler Blovat <tyler.blovat@uconn.edu>
2021-02-28 14:00:12 -0500

    Add a basic whole-number addition and subtraction worksheet

--- components/materials/Arithmetic/Arithmetic.tsx
+++ components/materials/Arithmetic/Arithmetic.tsx
@@ -3,6 +3,15 @@ import { MaterialsLink } from "../MaterialsLink/MaterialsLink";
 import styles from "./styles.module.scss";
 
 export const Arithmetic: React.FC = () => {
+  const testData = [
+    {
+      id: "krgmkmbkrmn56667",
+      title: "Addition and subtraction practice",
+      path: "arithmetic/worksheets/AddSub",
+      subject: "Arithmetic",
+      type: "worksheet",
+    },
+  ];
   return (
     <div className={styles.arithmetic}>
       <h1 className={styles.header}>Arithmetic</h1>
@@ -10,9 +19,9 @@ export const Arithmetic: React.FC = () => {
         Addition, subtraction, division, multiplication, fractions, negative
         numbers and more.
       </h2>
-      <p className={styles.flair}>
-        Sorry, we haven't added anything to our arithmetic course just yet!
-      </p>
+      {testData.map((el: any) => (
+        <MaterialsLink id={el.id} title={el.title} path={el.path} />
+      ))}
     </div>
   );
 };
---
+++ pages/materials/arithmetic/worksheets/AddSub/index.tsx
@@ -0,0 +1,21 @@
+import Head from "next/head";
+import { Navbar } from "../../../../../components/navigation/Navbar/Navbar";
+import { AddSub } from "../../../../../components/materials/documents/arithmetic/worksheets/AddSub/AddSub";
+import { Footer } from "../../../../../components/Footer/Footer";
+
+export default function AddSubPage() {
+  return (
+    <main>
+      <Head>
+        <title>Addition & Subtraction Practice Problems</title>
+        <meta
+          name="description"
+          content="Practice adding and subtracting whole numbers"
+        />
+      </Head>
+      <Navbar />
+      <AddSub />
+      <Footer />
+    </main>
+  );
+}
