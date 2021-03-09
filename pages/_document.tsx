ae2ce52d7b60470f537268fa20698c9ea06ad4a5
Tyler Blovat <tyler.blovat@uconn.edu>
2021-03-09 13:50:01 -0500

    Fix

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -27,16 +27,17 @@ export default class MyDocument extends Document {
           <script
             async
             src="https://www.googletagmanager.com/gtag/js?id=UA-189510484-1"
-          ></script>
+          />
           <script
             dangerouslySetInnerHTML={{
               __html: `
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-
-  gtag('config', 'UA-189510484-1');
-  `,
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'UA-189510484-1', {
+            page_path: window.location.pathname,
+          });
+          `,
             }}
           />
         </Head>
