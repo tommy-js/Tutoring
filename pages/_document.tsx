d02c276f3b373e6bd5f9b285ee1e0776fdfedd83
Tyler Blovat <tyler.blovat@uconn.edu>
2021-03-09 13:45:11 -0500

    Add google analytics ad tag

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -24,6 +24,21 @@ export default class MyDocument extends Document {
           `,
             }}
           />
+          <script
+            async
+            src="https://www.googletagmanager.com/gtag/js?id=UA-189510484-1"
+          ></script>
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-189510484-1');
+  `,
+            }}
+          />
         </Head>
         <body>
           <Main />
