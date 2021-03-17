df3ba1c46099396b34ba5e2fb664f10fbbf271f6
Tyler Blovat <tyler.blovat@uconn.edu>
2021-03-17 14:45:28 -0400

    Rewrite main paragraph

--- components/front_page/Introduction/Introduction.tsx
+++ components/front_page/Introduction/Introduction.tsx
@@ -9,11 +9,13 @@ export const Introduction: React.FC = () => {
       </h2>
 
       <p className={styles.text}>
-        I began my tutoring career at Dutchess Community College back in 2016.
-        At the time, I mostly helped struggling new college students with their
-        algebra and trigonometry, although I did tutor some calculus. I
-        absolutely fell in love with tutoring and now believe that it's my
-        calling in life.
+        I began my tutoring career in college back in 2016, mostly helping new
+        students with their algebra and trigonometry. At the time, I really
+        wanted to become a mathematical researcher. While I still love studying
+        math in my free time, I see now that tutoring is my true calling in
+        life. Spending so much time helping out new students made me realize
+        that there are many people who are in need of low-stress, approachable
+        math tutoring, and who can't afford to pay $80 per session.
       </p>
       <p className={styles.text}>
         I created Functional Tutoring in order to continue doing what I love:
