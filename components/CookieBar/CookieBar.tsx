7690d396b8293d4a2167782423480379a25060bc
Tyler Blovat <tyler.blovat@uconn.edu>
2021-03-17 17:27:57 -0400

    Add cookie bar

---
+++ components/CookieBar/CookieBar.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import styles from "./styles.module.scss";
+
+export const CookieBar: React.FC = () => {
+  return (
+    <div className={styles.cookie_bar}>
+      <p>Cookie Bar</p>
+    </div>
+  );
+};
