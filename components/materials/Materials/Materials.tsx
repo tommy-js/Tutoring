07a6d21b2d56a70789c90f64bc4cd35e20f05906
Tyler Blovat <tyler.blovat@uconn.edu>
2021-03-17 13:52:34 -0400

    Front page mobile changes

--- components/materials/Materials/Materials.tsx
+++ components/materials/Materials/Materials.tsx
@@ -1,8 +1,5 @@
 import React from "react";
-import { WelcomeHeader } from "../WelcomeHeader/WelcomeHeader";
-import { CourseMaterials } from "../CourseMaterials/CourseMaterials";
 import { FullCourseList } from "../FullCourseList/FullCourseList";
-import { ViewAllMaterials } from "../ViewAllMaterials/ViewAllMaterials";
 import styles from "./styles.module.scss";
 
 export const Materials: React.FC = () => {
