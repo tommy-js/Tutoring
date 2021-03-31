88e34660516c22a0d9d0c2781f0bcc9780943892
Tyler Blovat <tyler.blovat@uconn.edu>
2021-03-31 19:50:17 -0400

    Draft solutions to tomorrow and next day problem set

--- components/blog/posts/daily_posts/DailyPost4121/DailyPost4121.tsx
+++ components/blog/posts/daily_posts/DailyPost4121/DailyPost4121.tsx
@@ -9,8 +9,28 @@ const katex = require("katex");
 
 export const DailyPost4121: React.FC = () => {
   const problem1 = "f(x)=x^3-\\frac{4}{x^2}+5x-5";
-  const problem2 = "\\frac{x^2-4}{x+2}";
-  const problem3 = "\\sum_{n=1}^{\\infty}\\frac{3^n}{n}(5x-3)^n";
+  const problem2 = "f(x)=5x^2\\cos(x)";
+  const problem3 = "\\sum_{n=1}^{\\infty}\\frac{3^n}{n}(5x-25)^n";
+
+  const solution1_1 = "\\frac{4}{x^2}";
+  const solution1_2 = "x\\neq 0";
+
+  const solution2_1 = "(fg)'(x)=f(x)g'(x)+f'(x)g(x)";
+  const solution2_2 = "f'(x)=(5x^2)'\\cos(x)+5x^2(\\cos(x))'";
+  const solution2_3 = "(5x^2)'=10x";
+  const solution2_4 = "(\\cos(x))'=-\\sin(x)";
+  const solution2_5 = "f'(x)=10x\\cos(x)-5x^2\\sin(x)";
+
+  const solution3_1 = "L<1";
+  const solution3_2 = "\\lim_{n\\to \\infty}|\\frac{a_{n+1}}{a_n}|";
+  const solution3_3 =
+    "L=\\lim_{n\\to \\infty}|\\frac{3^{n+1}(5x-25)^{n+1}}{n+1}\\cdot\\frac{n}{3^n(5x-25)^n}|";
+  const solution3_4 = "\\lim_{n\\to\\infty}|\\frac{3x(5x-25)}{n+1}|";
+  const solution3_5 = "3|5x-25|";
+  const solution3_6 = "3|5x-25|<1";
+  const solution3_7 = "15|x-5|<1";
+  const solution3_8 = "|x-5|<\\frac{1}{15}";
+  const solution3_9 = "R=\\frac{1}{15}";
 
   return (
     <div className={styles.daily_post}>
@@ -38,26 +58,37 @@ export const DailyPost4121: React.FC = () => {
           <div className={styles.problem_solution}>
             <h3 className={styles.micro_header}>Solution</h3>
             <div className={styles.solution}>
-              <p className={styles.text}></p>
-              <p className={styles.text}></p>
-              <p className={styles.text}></p>
+              <p className={styles.text}>
+                In order to find where the function f(x) is continuous we only
+                need to look for the points at which it is defined. This means
+                we're looking for points of discontinuity. The only place where
+                this happens is in the <InlineMath>{solution1_1}</InlineMath>{" "}
+                term. When x=0 here the function is no longer defined. This
+                means that f(x) is continuous for all values{" "}
+                <InlineMath>{solution1_2}</InlineMath>.
+              </p>
             </div>
           </div>
         </div>
         <div className={styles.problem}>
           <div className={styles.problem_header}>
-            Find all points on the graph of the function{" "}
-            <InlineMath>{problem2}</InlineMath> where the tangent line is
-            parallel to the tangent line at x=4.
+            Take the derivative of <InlineMath>{problem2}</InlineMath>.
           </div>
           <div className={styles.problem_solution}>
             <h3 className={styles.micro_header}>Solution</h3>
             <div className={styles.solution}>
-              <p className={styles.text}></p>
-              <p className={styles.text}></p>
-              <p className={styles.text}></p>
-              <p className={styles.text}></p>
-              <p className={styles.text}></p>
+              <p className={styles.text}>
+                Here the most straightforward way of taking the derivative is to
+                use the product rule, which states that{" "}
+                <InlineMath>{solution2_1}</InlineMath>. So, we can rewrite our
+                derivative as <InlineMath>{solution2_2}</InlineMath>.
+              </p>
+              <p className={styles.text}>
+                From here this problem is easy to solve. We know that{" "}
+                <InlineMath>{solution2_3}</InlineMath> and{" "}
+                <InlineMath>{solution2_4}</InlineMath>. So, putting this all
+                together, we get that <InlineMath>{solution2_5}</InlineMath>
+              </p>
             </div>
           </div>
         </div>
@@ -69,10 +100,28 @@ export const DailyPost4121: React.FC = () => {
           <div className={styles.problem_solution}>
             <h3 className={styles.micro_header}>Solution</h3>
             <div className={styles.solution}>
-              <p className={styles.text}></p>
-              <p className={styles.text}></p>
-              <p className={styles.text}></p>
-              <p className={styles.text}></p>
+              <p className={styles.text}>
+                We'll use the Ratio Test to solve this problem. This test states
+                that if <InlineMath>{solution3_1}</InlineMath> for{" "}
+                <InlineMath>{solution3_2}</InlineMath>, then the series is
+                absolutely convergent. So, plugging in our problem, we get{" "}
+                <BlockMath>{solution3_3}</BlockMath>
+              </p>
+              <p className={styles.text}>
+                By simplification, this can be expressed as{" "}
+                <InlineMath>{solution3_4}</InlineMath>. Now, we know that the
+                limit here tends to 3 as n goes to infinity. This is because the
+                n terms become closer and closer as they become large. We can
+                therefore rewrite this expression as{" "}
+                <InlineMath>{solution3_5}</InlineMath>.
+              </p>
+              <p className={styles.text}>
+                We know that when <InlineMath>{solution3_6}</InlineMath>, the
+                series converges. Therefore, factoring out a 5, we get{" "}
+                <InlineMath>{solution3_7}</InlineMath>, which once we divide
+                gives us <InlineMath>{solution3_8}</InlineMath>. We then get our
+                final answer, which is <InlineMath>{solution3_9}</InlineMath>.
+              </p>
             </div>
           </div>
         </div>
--- components/blog/posts/daily_posts/DailyPost4221/DailyPost4221.tsx
+++ components/blog/posts/daily_posts/DailyPost4221/DailyPost4221.tsx
@@ -14,9 +14,25 @@ export const DailyPost4221: React.FC = () => {
   const problem2_4 = "-(7)^2";
 
   const problem3_1 = "\\sqrt{9}";
-  const problem3_2 = "\\sqrt{3\\cdot 3}";
+  const problem3_2 = "\\sqrt{5\\cdot 5}";
   const problem3_3 = "\\sqrt{16}";
-  const problem3_4 = "\\sqrt{9^2}";
+  const problem3_4 = "\\sqrt{4^2}";
+
+  const solution2_1 = "a^2=a\\times a";
+  const solution2_2 = "4^2=4\\times 4 = 16";
+  const solution2_3 = "(\\frac{5}{2})(\\frac{5}{2})=\\frac{25}{4}";
+  const solution2_4 = "(-6)(-6)";
+  const solution2_5 = "(-6)^2=36";
+  const solution2_6 = "-(7)^2=-(7)(7)";
+  const solution2_7 = "-(7)^2=-(7)(7)=-49";
+
+  const solution3_1 = "3\\times 3 = 9";
+  const solution3_2 = "\\sqrt{9}=3";
+  const solution3_3 = "\\sqrt{5^2}";
+  const solution3_4 = "\\sqrt{5\\cdot 5}=5";
+  const solution3_5 = "4\\times 4 = 16";
+  const solution3_6 = "\\sqrt{16}=4";
+  const solution3_7 = "\\sqrt{4^2}=4";
 
   return (
     <div className={styles.daily_post}>
@@ -43,9 +59,20 @@ export const DailyPost4221: React.FC = () => {
           <div className={styles.problem_solution}>
             <h3 className={styles.micro_header}>Solution</h3>
             <div className={styles.solution}>
-              <p className={styles.text}></p>
-              <p className={styles.text}></p>
-              <p className={styles.text}></p>
+              <p className={styles.text}>
+                What we need to do here is find all the values that divide 34
+                and 68. Then we will compare these values to find the largest
+                one, which will be our greatest common denominator. So, for 34
+                we know that it is divisible by 2 and 17. Now, we don't need to
+                find all the divisors of 68 because we're only looking for terms
+                that divide both numbers. We only need to test the numbers that
+                divide 34.
+              </p>
+              <p className={styles.text}>
+                Interestingly, we have that 68 is divisible by both terms.
+                68/2=34 and 68/17=4. This means that the greatest common factor
+                of both 34 and 68 is 17.
+              </p>
             </div>
           </div>
         </div>
@@ -59,10 +86,34 @@ export const DailyPost4221: React.FC = () => {
           <div className={styles.problem_solution}>
             <h3 className={styles.micro_header}>Solution</h3>
             <div className={styles.solution}>
-              <p className={styles.text}></p>
-              <p className={styles.text}></p>
-              <p className={styles.text}></p>
-              <p className={styles.text}></p>
+              <p className={styles.text}>
+                We start with <InlineMath>{problem2_1}</InlineMath>. Remember
+                that <InlineMath>{solution2_1}</InlineMath>, so,{" "}
+                <InlineMath>{solution2_2}</InlineMath>.
+              </p>
+              <p className={styles.text}>
+                Our second problem is a bit more complicated. When we square
+                fractions we need to apply fraction multiplication rules.
+                Multiplying fractions requires us to multiply across the
+                numerator and denominator of the fraction. So, for{" "}
+                <InlineMath>{problem2_2}</InlineMath>, we have{" "}
+                <InlineMath>{solution2_3}</InlineMath>.
+              </p>
+              <p className={styles.text}>
+                For <InlineMath>{problem2_3}</InlineMath>, we need to keep in
+                mind the negative value and pay attention to the parentheses. As
+                in our first example, this can be rewritten as{" "}
+                <InlineMath>{solution2_4}</InlineMath>. Since a negative times a
+                negative gives a positive, this simplifies down to{" "}
+                <InlineMath>{solution2_5}</InlineMath>.
+              </p>
+              <p className={styles.text}>
+                The final problem here is another slightly tricky one. The
+                negative value is outside the parentheses, so it can be
+                rewritten as <InlineMath>{solution2_6}</InlineMath>. Now it's
+                simply a matter of multiplying the values out. Our final answer
+                is <InlineMath>{solution2_7}</InlineMath>.
+              </p>
               <p className={styles.text}></p>
             </div>
           </div>
@@ -77,10 +128,34 @@ export const DailyPost4221: React.FC = () => {
           <div className={styles.problem_solution}>
             <h3 className={styles.micro_header}>Solution</h3>
             <div className={styles.solution}>
-              <p className={styles.text}></p>
-              <p className={styles.text}></p>
-              <p className={styles.text}></p>
-              <p className={styles.text}></p>
+              <p className={styles.text}>
+                Starting with <InlineMath>{problem3_1}</InlineMath>, we're
+                looking for a number that when multiplied with itself gives the
+                value 9. Since <InlineMath>{solution3_1}</InlineMath>, 3 is the
+                number we're looking for. So finally,{" "}
+                <InlineMath>{solution3_2}</InlineMath>.
+              </p>
+              <p className={styles.text}>
+                For the second problem, we have{" "}
+                <InlineMath>{problem3_2}</InlineMath>. Since we're multiplying
+                the same number together within the radical, we can rewrite this
+                as <InlineMath>{solution3_3}</InlineMath>. Now, because the
+                square and square root are inverse operations, they cancel out,
+                leaving us with <InlineMath>{solution3_4}</InlineMath>.
+              </p>
+              <p className={styles.text}>
+                Moving on to <InlineMath>{problem3_3}</InlineMath>, we're again
+                looking for a number than when multiplied by itself gives the
+                value 16. This is easily found to be 4, since{" "}
+                <InlineMath>{solution3_5}</InlineMath>. So, this gives us our
+                answer <InlineMath>{solution3_6}</InlineMath>.
+              </p>
+              <p className={styles.text}>
+                For the final problem, we again keep in mind that the square
+                root and squaring a number are opposite functions. That is, one
+                cancels out the other. So,{" "}
+                <InlineMath>{solution3_7}</InlineMath>.
+              </p>
             </div>
           </div>
         </div>
