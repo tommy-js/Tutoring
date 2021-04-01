de64c1bbc932a6387b242a5c02111cc57c1f5955
Tyler Blovat <tyler.blovat@uconn.edu>
2021-03-31 23:16:58 -0400

    Add more problem solutions

--- components/blog/posts/daily_posts/DailyPost4621/DailyPost4621.tsx
+++ components/blog/posts/daily_posts/DailyPost4621/DailyPost4621.tsx
@@ -10,9 +10,18 @@ const katex = require("katex");
 export const DailyPost4621: React.FC = () => {
   const problem1 = "f(x)=4x^3-2x^2-x-6";
 
-  const problem2 = "f(x)=5x^3+2x-7";
+  const problem2 = "f(x)=5x^3-2x-7";
 
-  const problem3 = "f(x)=3x^5-2x^3+8x^2-7";
+  const problem3 = "f(x)=3x^4-2x^3+8x^2-7";
+
+  const solution1_1 = "4(-5)^3-2(-5)^2+5-6=-500-50+5-6=-551";
+  const solution1_2 = "4(6)^3-2(6)^2+5-6=864-72+5-6=791";
+
+  const solution2_1 = "f'(x)=15x^2-2";
+  const solution2_2 = "15x^2-2=0";
+  const solution2_3 = "15x^2=2";
+  const solution2_4 = "x^2=\\frac{2}{15}";
+  const solution2_5 = "x=\\pm\\sqrt{\\frac{2}{15}}";
 
   return (
     <div className={styles.daily_post}>
@@ -41,22 +50,47 @@ export const DailyPost4621: React.FC = () => {
           <div className={styles.problem_solution}>
             <h3 className={styles.micro_header}>Solution</h3>
             <div className={styles.solution}>
-              <p className={styles.text}></p>
-              <p className={styles.text}></p>
-              <p className={styles.text}></p>
+              <p className={styles.text}>
+                The IVT shows that if a function has values of opposite signs on
+                some interval, then there must be a root within that interval.
+                So to determine whether or not there is a solution to our
+                function we just need to calculate the function values at each
+                endpoint.
+              </p>
+              <p className={styles.text}>
+                For x=-5, we get <InlineMath>{solution1_1}</InlineMath>. For x=6
+                we calculate <InlineMath>{solution1_2}</InlineMath>. Since we
+                see a sign-change we know there somewhere on the interval there
+                must be a zero.
+              </p>
             </div>
           </div>
         </div>
         <div className={styles.problem}>
           <div className={styles.problem_header}>
-            Find the maximum and minimum of the function{" "}
+            Find the local maximum and minimum of the function{" "}
             <InlineMath>{problem2}</InlineMath> using derivatives.{" "}
           </div>
           <div className={styles.problem_solution}>
             <h3 className={styles.micro_header}>Solution</h3>
             <div className={styles.solution}>
-              <p className={styles.text}></p>
-              <p className={styles.text}></p>
+              <p className={styles.text}>
+                While there are several ways to find the maximum and minimum of
+                the function, we are going to use methods of calculus. We will
+                take the first derivative and then find where this derivative
+                equals zero, which will find where the slope flattens out. That
+                gives us the maximum and minimum.
+              </p>
+              <p className={styles.text}>
+                So, taking the derivative of the function, we get{" "}
+                <InlineMath>{solution2_1}</InlineMath>. Now, we solve for the
+                roots by setting <InlineMath>f'(x)=0</InlineMath>. So, we solve{" "}
+                <InlineMath>{solution2_2}</InlineMath>. Adding 2 to each side,
+                this becomes <InlineMath>{solution2_3}</InlineMath>. Then,
+                dividing by 15, we get <InlineMath>{solution2_4}</InlineMath>.
+                Finally, take the square root, and don't forget to include a
+                plus and minus: <InlineMath>{solution2_5}</InlineMath>.
+              </p>
               <p className={styles.text}></p>
               <p className={styles.text}></p>
               <p className={styles.text}></p>
@@ -71,7 +105,10 @@ export const DailyPost4621: React.FC = () => {
           <div className={styles.problem_solution}>
             <h3 className={styles.micro_header}>Solution</h3>
             <div className={styles.solution}>
-              <p className={styles.text}></p>
+              <p className={styles.text}>
+                Finding the inflection points of a function requires us to take
+                the second derivative.
+              </p>
               <p className={styles.text}></p>
               <p className={styles.text}></p>
               <p className={styles.text}></p>
