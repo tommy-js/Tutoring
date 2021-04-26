b2c865080c218497bc9c3c493f615d8124784de5
Tyler Blovat <tyler.blovat@uconn.edu>
2021-04-26 10:52:08 -0400

    Write daily problem

--- components/blog/posts/daily_posts/DailyPost42621/DailyPost42621.tsx
+++ components/blog/posts/daily_posts/DailyPost42621/DailyPost42621.tsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import ReactPlayer from "react-player";
 import { BlockMath, InlineMath } from "react-katex";
+import Link from "next/link";
 import { SignUpEmailForm } from "../SignUpEmailForm/SignUpEmailForm";
 import styles from "./styles.module.scss";
 import "katex/dist/katex.min.css";
@@ -9,31 +10,32 @@ const katex = require("katex");
 
 export const DailyPost42621: React.FC = () => {
   const [problem1Color, setProblem1Color] = useState("transparent");
+  const [linkColor, setLinkColor] = useState("transparent");
   const [problem1Shadow, setProblem1Shadow] = useState("0 0 7px #000");
-  const [problem2Color, setProblem2Color] = useState("transparent");
-  const [problem2Shadow, setProblem2Shadow] = useState("0 0 7px #000");
-  const [problem3Color, setProblem3Color] = useState("transparent");
-  const [problem3Shadow, setProblem3Shadow] = useState("0 0 7px #000");
+
+  const answer2 = "x=\\sqrt{\\frac{7}{5}}";
+  const answer3 = "x=\\sqrt[3]{\\frac{375}{32}}";
+  const answer4 = "x=\\sqrt[3]{\\frac{274}{87}}";
+
+  const solution1 = "h=\\frac{25}{2x^2}";
+  const solution2 = "C(x)=16x^2+\\frac{375}{x}";
+  const solution3 = "C'(x)=32x-\\frac{375}{x^2}";
+  const solution4 = "32x-\\frac{375}{x^2}=0";
+  const solution5 = "32x=\\frac{375}{x^2}";
+  const solution6 = "32x^3=375";
+  const solution7 = "x^3=\\frac{375}{32}";
+  const solution8 = "x=\\sqrt[3]{\\frac{375}{32}}";
 
   function revealProblem1() {
     setProblem1Color("#3c3b3b");
+    setLinkColor("#0b2cc2");
     setProblem1Shadow("none");
-  }
-
-  function revealProblem2() {
-    setProblem2Color("#3c3b3b");
-    setProblem2Shadow("none");
-  }
-
-  function revealProblem3() {
-    setProblem3Color("#3c3b3b");
-    setProblem3Shadow("none");
   }
 
   return (
     <div className={styles.daily_post}>
-      <h1 className={styles.header}>Calculus</h1>
-      <h2 className={styles.subheader}>Posted on [DATE]</h2>
+      <h1 className={styles.header}>Calculus | Box Optimization</h1>
+      <h2 className={styles.subheader}>Posted on 4/26/21</h2>
       <div className={styles.video_block}>
         <ReactPlayer
           url="https://youtu.be/LgYmGkCuPTQ"
@@ -50,13 +52,25 @@ export const DailyPost42621: React.FC = () => {
       <div className={styles.solutions_block}>
         <h3 className={styles.solutions_header}>Problems & Solutions</h3>
         <div className={styles.problem}>
-          <div className={styles.problem_header}>1.</div>
+          <div className={styles.problem_header}>
+            A rectangular box with an open top must have a total volume of{" "}
+            <InlineMath>25m^3</InlineMath>. The length of its base is twice that
+            of its width. Material for its base costs $8 per square meter.
+            Material for the sides costs $5 per meter. Find the total cost of
+            the material for the cheapest box.
+          </div>
           <div className={styles.context_container}>
             <ol className={styles.multiple_choice}>
-              <li className={styles.individual_choice}>[ANS1]</li>
-              <li className={styles.individual_choice}>[ANS2]</li>
-              <li className={styles.individual_choice}>[ANS3]</li>
-              <li className={styles.individual_choice}>[ANS4]</li>
+              <li className={styles.individual_choice}>x=14</li>
+              <li className={styles.individual_choice}>
+                <InlineMath>{answer2}</InlineMath>
+              </li>
+              <li className={styles.individual_choice}>
+                <InlineMath>{answer3}</InlineMath>
+              </li>
+              <li className={styles.individual_choice}>
+                <InlineMath>{answer4}</InlineMath>
+              </li>
             </ol>
           </div>
           <div className={styles.problem_solution}>
@@ -71,78 +85,99 @@ export const DailyPost42621: React.FC = () => {
               className={styles.solution}
               style={{ color: problem1Color, textShadow: problem1Shadow }}
             >
-              <div className={styles.answer}>
-                <h4 className={styles.answer_header}>Answer: </h4>
-                <p className={styles.answer_text}></p>
-              </div>
               <div className={styles.details}>
                 <h4 className={styles.details_header}>Explanation: </h4>
-                <p className={styles.text}></p>
-                <p className={styles.text}></p>
-                <p className={styles.text}></p>
-                <p className={styles.text}></p>
-                <p className={styles.text}></p>
+                <p className={styles.text}>
+                  The first thing we want to do is draw the box. This should
+                  always be our first step, as it will help us visualize what
+                  we're trying to solve for.
+                </p>
+                <p className={styles.text}>
+                  After drawing the box, we want to determine a cost function.
+                  We see that since the box is longer than it is wide, and the
+                  cost of the base is $8 per square meter, the function for our
+                  base's cost is <InlineMath>C=8*x*2x</InlineMath>. Now we
+                  really need to find the cost of the sides, and then we'll have
+                  figured out our function.
+                </p>
+                <p className={styles.text}>
+                  Since we have the sides costing $5 per square meter, we know
+                  that the narrower sides are going to cost{" "}
+                  <InlineMath>C=5*x*h</InlineMath>, where h is the height of the
+                  box, and the wider sides are going to run us{" "}
+                  <InlineMath>C=5*2x*h</InlineMath>.
+                </p>
+                <p className={styles.text}>
+                  The total cost of the box, after a hefty amount of
+                  simplification, can be expressed as the function{" "}
+                  <InlineMath>C=16x^2+30xh</InlineMath>. We have a problem,
+                  though; we don't know how to optimize with respect to two
+                  variables, x and h. Luckily, we have a trick to figure out
+                  what the value of h is.
+                </p>
+                <p className={styles.text}>
+                  Since the question told us that the volume must be 25 cubic
+                  meters, and we know that{" "}
+                  <InlineMath>Volume=Base*Width*Height</InlineMath>, we see that{" "}
+                  <InlineMath>x*2x*h=25</InlineMath>. Since we have h here, we
+                  can easily solve for h in terms of x. Dividing both sides by{" "}
+                  <InlineMath>2x^2</InlineMath>, we get that{" "}
+                  <InlineMath>{solution1}</InlineMath>. Thus, we can substitute
+                  this value of h into our cost function to get{" "}
+                  <InlineMath>{solution2}</InlineMath>. This is, of course,
+                  after a bit of simplification.
+                </p>
+                <p className={styles.text}>
+                  Now that we have a function in terms of x, we want to figure
+                  out the dimensions of the most cost-effective box we can buy.
+                  To do this, we'll find the minimums of the function, which
+                  requires us to find its derivative and solve for zero. In
+                  other words, we're looking for the critical points of the
+                  function and then determining the minimum. Taking the
+                  derivative of this function is easy; it's a simple Power Rule
+                  problem. If you need a refresher on the Power Rule,{" "}
+                  <Link href="https://functionaltutoring.com/materials/calculus1/methods/Derivatives">
+                    <a
+                      className={styles.link}
+                      style={{
+                        color: linkColor,
+                        textShadow: problem1Shadow,
+                      }}
+                    >
+                      we have articles on this
+                    </a>
+                  </Link>{" "}
+                  in our calculus page.
+                </p>
+                <p className={styles.text}>
+                  After taking the derivative, we're left with{" "}
+                  <InlineMath>{solution3}</InlineMath>. Since we're looking for
+                  the critical points, we take this function and set it equal to
+                  zero. <BlockMath>{solution4}</BlockMath>
+                </p>
+                <p className={styles.text}>
+                  We add our fraction to both sides and get{" "}
+                  <InlineMath>{solution5}</InlineMath>. Now, multiplying each
+                  side by <InlineMath>x^2</InlineMath>, this gives us{" "}
+                  <InlineMath>{solution6}</InlineMath>. At this point solving
+                  for x should be fairly easy and straightforward. We simply
+                  divide both sides by 32 to get{" "}
+                  <InlineMath>{solution7}</InlineMath>, and then take the third
+                  root of x.
+                </p>
+                <p className={styles.text}>
+                  Thus, our critical point is{" "}
+                  <InlineMath>{solution8}</InlineMath>. Because we only get one
+                  critical point, it's very likely that this is the minimum of
+                  our function. We could use the Second Derivative Test to make
+                  sure this is accurate, but we could also simply use what we
+                  know about parabolas to make sure our answer makes sense.
+                  Since we have a positive x-squared term on our cost function,
+                  we know that it is concave up, and so it has one global
+                  minimum value and no global maximum. Thus, our answer must be
+                  correct, and we are finished.
+                </p>
               </div>
-            </div>
-          </div>
-        </div>
-        <div className={styles.problem}>
-          <div className={styles.problem_header}>2.</div>
-          <div className={styles.context_container}>
-            <ol className={styles.multiple_choice}>
-              <li className={styles.individual_choice}>[ANS1]</li>
-              <li className={styles.individual_choice}>[ANS2]</li>
-              <li className={styles.individual_choice}>[ANS3]</li>
-              <li className={styles.individual_choice}>[ANS4]</li>
-            </ol>
-          </div>
-          <div className={styles.problem_solution}>
-            <h3 className={styles.micro_header}>Solution</h3>
-            <button
-              className={styles.solution_button}
-              onClick={() => revealProblem2()}
-            >
-              View Solution
-            </button>
-            <div
-              className={styles.solution}
-              style={{ color: problem2Color, textShadow: problem2Shadow }}
-            >
-              <p className={styles.text}></p>
-              <p className={styles.text}></p>
-              <p className={styles.text}></p>
-              <p className={styles.text}></p>
-              <p className={styles.text}></p>
-            </div>
-          </div>
-        </div>
-        <div className={styles.problem}>
-          <div className={styles.problem_header}>3.</div>
-          <div className={styles.context_container}>
-            <ol className={styles.multiple_choice}>
-              <li className={styles.individual_choice}>[ANS1]</li>
-              <li className={styles.individual_choice}>[ANS2]</li>
-              <li className={styles.individual_choice}>[ANS3]</li>
-              <li className={styles.individual_choice}>[ANS4]</li>
-            </ol>
-          </div>
-          <div className={styles.problem_solution}>
-            <h3 className={styles.micro_header}>Solution</h3>
-            <button
-              className={styles.solution_button}
-              onClick={() => revealProblem3()}
-            >
-              View Solution
-            </button>
-            <div
-              className={styles.solution}
-              style={{ color: problem3Color, textShadow: problem3Shadow }}
-            >
-              <p className={styles.text}></p>
-              <p className={styles.text}></p>
-              <p className={styles.text}></p>
-              <p className={styles.text}></p>
-              <p className={styles.text}></p>
             </div>
           </div>
         </div>
